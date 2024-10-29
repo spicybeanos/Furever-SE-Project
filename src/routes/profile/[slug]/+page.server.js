e6d4011c29827668d2ec733fd20f38f1fc985935
Aryan D <aryan.d.dalal@gmail.com>
2024-10-29 13:01:26 +0530

    profile get should work

--- src/routes/profile/[slug]/+page.server.js
+++ src/routes/profile/[slug]/+page.server.js
@@ -1,12 +1,33 @@
 //get the profile data
 import { error } from '@sveltejs/kit';
+const profile_getter = 'http://127.0.0.1:8080/api/profiles/';
 
-export function load({params}) {
+
+export async function load({ params }) {
     const slug = params.slug;
+    if (slug === "")
+        return {
+            pfp_url: "",
+            username: "Could not load profile",
+            bio: "Empty slug for username field"
+        };
+    try {
+        const resp = await fetch(profile_getter + slug, {
+            headers: { 'Content-Type': 'application/json' },
+            method: "GET",
+        });
+        const body = await resp.json();
+        return {
+            pfp_url: body.pfp_url,
+            username: body.username,
+            bio: body.bio
+        };
+    } catch (exc) {
+        return {
+            pfp_url: "",
+            username: "Error fetching user profile",
+            bio: exc
+        };
+    }
 
-    return {
-        pfp_url:"example/example_pfp.jpg",
-        username:"cat_lover_8008135 slug:"+slug,
-        bio:"i love cats and send posts about cats"
-    };
 }
